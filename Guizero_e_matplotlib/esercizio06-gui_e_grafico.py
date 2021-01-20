d7b2ec06e4a740aabf06d6f0b9e8925107166ebf
GabrieleFerrante <ferrante.gabriele.s25@liceoviconapoli.it>
2021-01-20 21:28:33 +0100

    Guizero e matplot lib, riorganizzazione del repository

---
+++ Guizero_e_matplotlib/esercizio06-gui_e_grafico.py
@@ -0,0 +1,42 @@
+from os import read
+from random import randint
+import matplotlib.pyplot as plt
+from guizero import *
+
+coordX = []
+coordY = []
+
+def data2coord():
+
+    f = open(box_dati.value, "r")
+
+    for line in f:
+        values = str(f.readline())
+        values = values.strip("\n")
+        values = values.split(",")
+        values = list(values)
+        coordX.append(int(values[0]))
+        coordY.append(int(values[1]))
+
+    f.close()
+
+    coordX.sort()
+    coordY.sort()
+
+def grafico():
+
+    data2coord()
+    plt.plot(coordX, coordY)
+    plt.savefig("grafico.png")
+
+    graph = Picture(app, image = "grafico.png")
+
+
+app = App(title="Lettura dati", width = 600, height = 600, bg = "#ffffff")
+
+label = Text(app, text = "Inserisci il nome del file da leggere:")
+box_dati = TextBox(app, width = 40)
+but = PushButton(app, command = grafico, text = "Genera")
+
+
+app.display()
